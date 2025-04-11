8fc085c74b6396570c3aefdfe825dcf7098fb42a
santimtz.github.io <185701@upslp.edu.mx>
2025-04-11 07:55:23 -0600

    Add files via upload

---
+++ Proyecto/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+
+    slidePerView: 1,
+    spacebewteen: 30,
+    grabCursor: true,
+    loop:true,
+    breakpoints : {
+        991: {
+            slidesPerView:4
+        }
+    }
+
+});
